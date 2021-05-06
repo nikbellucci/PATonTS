48a6751f33e8ee439dd0f0393637a41adfbaf96f
Niccolò Bellucci <niccolo.bellucci.dev@gmail.com>
2021-05-06 15:42:22 +0200

    Added JavaFX dependency

--- src/main/java/com/proginternet/App.java
+++ src/main/java/com/proginternet/App.java
@@ -1,17 +1,33 @@
 package com.proginternet;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  */
-public final class App {
-    private App() {
-    }
+public final class App extends Application {
+    // private App() {
+    // }
 
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Test maven");
+        // System.out.println("Test maven");
+        launch();
+    }
+
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 }
