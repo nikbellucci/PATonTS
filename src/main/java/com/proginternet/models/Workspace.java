20a80b8d9f6218e69021ed15a00ef5f82a206da3
Niccolò Bellucci <niccolo.bellucci.dev@gmail.com>
2021-05-09 18:36:50 +0200

    Added Workspace model

---
+++ src/main/java/com/proginternet/models/Workspace.java
@@ -0,0 +1,11 @@
+package com.proginternet.models;
+
+public class Workspace {
+    
+    private String id;
+    private String name;
+    private String description;
+    private String category;
+    private Activity activities;
+
+}
