ea62bb282fdfdc4be25f9d4608342a9ff7faf0fa
Niccolò Bellucci <niccolo.bellucci.dev@gmail.com>
2021-05-09 18:34:45 +0200

    Added Activity model

---
+++ src/main/java/com/proginternet/models/Activity.java
@@ -0,0 +1,13 @@
+package com.proginternet.models;
+
+public class Activity {
+    
+    private String id;
+    private String name;
+    private String description;
+    private String where;
+    private int availableSeats;
+    private Preference preferencies;
+
+    
+}
