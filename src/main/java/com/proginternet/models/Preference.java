dd9a26721631d478f2a4b2e9597da74f6f366e72
Niccolò Bellucci <niccolo.bellucci.dev@gmail.com>
2021-05-09 18:36:25 +0200

    Added Preference model

---
+++ src/main/java/com/proginternet/models/Preference.java
@@ -0,0 +1,11 @@
+package com.proginternet.models;
+
+import java.util.ArrayList;
+
+public class Preference {
+    
+    private String name;
+    private String description;
+    private ArrayList valueSelection;
+    
+}
